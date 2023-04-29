9a6216c4fdbbc5f8d3ea8511c1740268410661cd
Byambanaidan Otgontenger <unnaidan@gmail.com>
2023-04-29 18:26:23 +0800

    db schema

--- src/user/events/user-created.event.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '@prisma/client';
-
-export class UserCreatedEvent {
-    constructor(
-        public user: User
-    ) { }
-}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,31 +1,17 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { AddonModule } from '../addon/addon.module';
 import { AuthModule } from '../auth/auth.module';
 import { AuthenticateConstraint } from '../custom-validation-decorators/authenticate.decorator';
-import { IsUniqueConstraint } from '../custom-validation-decorators/is-unique.decorator';
-import { IsUsernameValidConstraint } from '../custom-validation-decorators/is-username-valid.decorator';
-import { SocialIconModule } from '../social-icon/social-icon.module';
-import { UserCreatedListener } from './listeners/user-created.listener';
-import { UserController } from './user.controller';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
 @Module({
     providers: [
         AuthenticateConstraint,
-        IsUsernameValidConstraint,
-        IsUniqueConstraint,
-        UserCreatedListener,
         UserResolver,
         UserService
     ],
-    controllers: [
-        UserController
-    ],
     imports: [
-        forwardRef(() => AddonModule),
-        forwardRef(() => AuthModule),
-        forwardRef(() => SocialIconModule)
+        forwardRef(() => AuthModule)
     ],
     exports: [
         UserService
